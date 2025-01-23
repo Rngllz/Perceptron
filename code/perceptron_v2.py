f5d06b23a6a43f30d5c4735e6651e13c822bb9de
Luca Mateo Rangel <lucamateorangel@hotmail.com>
2025-01-22 21:23:10 -0300

    02

--- code/perceptron_v2.py
+++ code/perceptron_v2.py
@@ -1,7 +1,8 @@
 import numpy as np
+# melhorias com o numpy, mais velocidade
 
-entradas = [1, 7 ,5]
-pesos = [0.8, 0.1, 0]
+entradas = np.array([1, 7 ,5])
+pesos = np.array([0.8, 0.1, 0])
 
 def soma(e, p):
     return e.dot(p)
